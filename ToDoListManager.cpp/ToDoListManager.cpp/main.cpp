8faaf14c08cd46ecca08cc4fc68caec65c64081b
Samuel Tsogbe <samueltsogbe@MacBook-Pro-85.local>
2024-10-02 21:08:09 -0700

    created a show menu

---
+++ ToDoListManager.cpp/ToDoListManager.cpp/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  ToDoListManager.cpp
+//
+//  Created by Samuel Tsogbe on 10/2/24.
+//
+
+#include <iostream>
+using namespace std;
+
+void addTask(int ,int , string);
+void showMenu();
+
+using namespace std;
+int main (){
+    bool running = true
+    
+
+}
+void showMenu(){
+    cout << "1. Add a task" << endl;
+    cout << "2. Remove a task" << endl;
+    cout << "3. Mark a task as completed" << endl;
+    cout << "4. View all tasks" << endl;
+    cout << "5. Exit" << endl;
+}
