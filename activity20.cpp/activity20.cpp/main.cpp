12746f3880ea8c929f84da5868d0300d0426cbe2
Samuel Tsogbe <samueltsogbe@MacBook-Pro-85.local>
2024-10-14 10:57:53 -0700

    dynamcatlly array

---
+++ activity20.cpp/activity20.cpp/main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  activity20.cpp
+//
+
+#include <iostream>
+uainf namespace std;
+
+int main(int argc, const char * argv[]) {
+    int size;
+    
+    cout << " enter the number of slott u want";
+    cin >> size;
+    
+    int* arr = new int[size];
+    
+    cout << " enter " << size << "integers:" << endl;
+    for ( int i = 0; i < size; i++){
+        cin >> arr[i];
+    }
+    
+    int  sum = 0;
+    for ( int i = 0; i < size; i++){
+       sum =+ arr[i];
+    }
+    
+    
+    cout << "The sum of the array is: " << sum << endl;
+    
+    delete[] arr;
+    return 0; 
+}
