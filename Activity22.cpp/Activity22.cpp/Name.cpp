ce3561c4288844dea92fdf04756bfea86f89cf7b
Samuel Tsogbe <samueltsogbe@MacBook-Pro-85.local>
2024-10-23 22:56:37 -0700

    made the varibale to hold the first name

--- Activity22.cpp/Activity22.cpp/Name.cpp
+++ Activity22.cpp/Activity22.cpp/Name.cpp
@@ -13,6 +13,9 @@ void extractFrstAndLastName(const std::string& fullName, std::string& firstName,
     // to find the first space
     size_t firstSPace = fullName.find(' ');
     
-    if (firstSPace != string::npos)
+    if (firstSPace != string::npos){
+        //subtracts teh 0 index to the the index of the first space
+        firstName = fullName.substr(0, firstSPace);
+    }
     
 }
