1ba5a59518120763a2e58ca59c815daf4ad02237
Samuel Tsogbe <samueltsogbe@MacBook-Pro-85.local>
2024-10-23 22:38:39 -0700

    created the files and declared my function in the header

---
+++ Activity22.cpp/Activity22.cpp/Name.h
@@ -0,0 +1,16 @@
+//
+//  Name.h
+//  Activity22.cpp
+//
+
+#ifndef NAME_H
+#define NAME_H
+
+#include <string>
+
+void extractFrstAndLastName(const std::string& fullName, std::strings& firstName, std:: lastName);
+
+
+
+#endif
+
