1eb3327c10f578e9159c91121e98271bdc4b09f1
Samuel Tsogbe <samueltsogbe@MacBook-Pro-85.local>
2024-11-18 22:35:52 -0800

    finished the main and also fixed retrun in all paths for the function

--- activity17.cpp/activity17.cpp/main.cpp
+++ activity17.cpp/activity17.cpp/main.cpp
@@ -6,8 +6,9 @@
 //
 
 #include <iostream>
+using namespace std;
 int linearsearch(const int arr[], int size, int value){
-    int index = o;
+    int index = 0;
     int position = -1;
     bool found = false;
     
@@ -17,9 +18,9 @@ int linearsearch(const int arr[], int size, int value){
             position = index;
         }
         index++;
-        return position;
     }
-    
+    return position;
+
 }
 int main(){
     
@@ -30,7 +31,7 @@ int main(){
     if (results == -1)
         cout << "you do not have a 3 in the arr";
     else {
-        cout << "we found it int the index "
+        cout << "we found it 3 in the index:" << results + 1;
     }
     
     
