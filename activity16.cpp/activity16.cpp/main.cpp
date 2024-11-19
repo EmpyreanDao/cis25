0bbe9f3a92ed3737d838b6488da622ccf84187b7
Samuel Tsogbe <samueltsogbe@MacBook-Pro-85.local>
2024-11-18 16:51:22 -0800

    swtiching activity 16 from array to vectors for simplicity

---
+++ activity16.cpp/activity16.cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  activity16.cpp
+//
+//  Created by Samuel Tsogbe on 11/18/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
